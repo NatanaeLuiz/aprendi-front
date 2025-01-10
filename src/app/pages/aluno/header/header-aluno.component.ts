4da77c2d23e398235772d6ff61821ba1901f9f00
Natanael Rolim <natanael.10@hotmail.com>
2025-01-10 19:15:13 -0300

    FEAT: Implementado redirecionamento de perfil logado

--- src/app/pages/aluno/header/header-aluno.component.ts
+++ src/app/pages/aluno/header/header-aluno.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../auth/login/service/auth.service';
 
 @Component({
   selector: 'app-header-aluno',
@@ -9,4 +10,9 @@ import { RouterLink } from '@angular/router';
 })
 export class HeaderAlunoComponent {
 
+  constructor(private authService: AuthService) {}
+
+  onClick() {
+    this.authService.logout();
+  }
 }
--- src/app/pages/auth/login/service/auth.service.ts
+++ src/app/pages/auth/login/service/auth.service.ts
@@ -10,9 +10,11 @@ export class AuthService {
     login(username: string, password: string) {
       if (username === 'admin' && password === '1234') {
         this.user = { username, role: 'admin' };
+        localStorage.setItem('userRole', 'admin'); // Persistir a role no localStorage
         return of(this.user);
       } else if (username === 'aluno' && password === '1234') {
         this.user = { username, role: 'aluno' };
+        localStorage.setItem('userRole', 'aluno'); // Persistir a role no localStorage
         return of(this.user);
       } else {
         throw new Error('Usuário ou senha inválidos');
@@ -25,5 +27,14 @@ export class AuthService {
   
     isAluno() {
       return this.user?.role === 'aluno';
+    }
+
+    getUserRole(): string | null {
+      return localStorage.getItem('userRole'); // Recuperar a role do localStorage
+    }
+
+    logout() {
+      this.user = null;
+      localStorage.removeItem('userRole');
     }
   }
