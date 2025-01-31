c2d545e01815c2a752a3e9b7670ef3c38a54bfd7
demillyferreira <demilly_@hotmail.com>
2025-01-30 21:13:24 -0300

    listar todas as instituicoes

--- src/app/pages/admin/gerenciar/instituicao/cadastro-instituicao/cadastro-instituicao.component.ts
+++ src/app/pages/admin/gerenciar/instituicao/cadastro-instituicao/cadastro-instituicao.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { InstituicaoService } from './instituicao.service';
+import { InstituicaoService } from '../service/instituicao.service';
 import { FormsModule, NgForm, NgModel } from '@angular/forms';
 import { Instituicao } from '../model/instituicao.model';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-instituicao',
   templateUrl: './cadastro-instituicao.component.html',
   styleUrl: './cadastro-instituicao.component.css',
   standalone: true,
-  imports: [FormsModule, CommonModule]
+  imports: [FormsModule, CommonModule, RouterLink]
 })
 export class CadastroInstituicaoComponent implements OnInit {
 
@@ -23,10 +24,28 @@ export class CadastroInstituicaoComponent implements OnInit {
   isLoading = false;
   mensagemSucesso: string | null = null;
   mensagemErro: string | null = null;
+  instituicoes: Instituicao[] = [];
 
   constructor(private instituicaoService: InstituicaoService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.listarTodasInsituicoes();
+  }
+
+  private listarTodasInsituicoes() {
+    this.isLoading = true;
+    this.instituicaoService.listarInstituicoes().subscribe({
+      next: (instituicoes) => {
+        this.instituicoes = instituicoes;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.mensagemErro = 'Erro ao listar todas as instituições.';
+        console.error(error);
+        this.isLoading = false;
+      }
+    });
+  }
 
   salvarInstituicao(form: NgForm): void {
     if (form.invalid) {
