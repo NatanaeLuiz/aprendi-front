81b478652b2c0e55190bfdb0c57d1f75bd77742d
Natanael Rolim <natanael.10@hotmail.com>
2025-02-02 16:27:20 -0300

    FIX: Ajustes card cursos

--- src/app/pages/aluno/home/home.component.ts
+++ src/app/pages/aluno/home/home.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from '../../../services/backend.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
+
+  cardCursos: String[] = ['curso1', 'curso2', 'curso2', 'curso4']
+
+  //TODO: Consumir Backend serviço de todos cursos ATIVOS da instituição X (O serviço deve retornar o professor do modulo)
 
   constructor(private backEnd: BackendService){
 
--- src/app/pages/aluno/meus-cursos/meus-cursos.component.ts
+++ src/app/pages/aluno/meus-cursos/meus-cursos.component.ts
@@ -1,12 +1,16 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-meus-cursos',
-  imports: [RouterLink],
+  imports: [RouterLink, NgFor],
   templateUrl: './meus-cursos.component.html',
   styleUrl: './meus-cursos.component.css'
 })
 export class MeusCursosComponent {
 
+  cardCursos: String[] = ['curso1', 'curso2', 'curso2', 'curso4']
+
+  //TODO: Consumir Backend serviço de todos cursos do usuario X
 }
