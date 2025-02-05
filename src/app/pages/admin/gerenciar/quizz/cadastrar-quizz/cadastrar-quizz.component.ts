effe00c7d828da3d6d268da9b7af9422ebf12f3c
amandaolmr <amandaolmr@users.noreply.github.com>
2025-02-05 20:01:44 -0300

    ajustanto  a tela de cadstro de quizz

--- src/app/pages/admin/gerenciar/quizz/cadastrar-quizz/cadastrar-quizz.component.ts
+++ src/app/pages/admin/gerenciar/quizz/cadastrar-quizz/cadastrar-quizz.component.ts
@@ -2,16 +2,33 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { BackendService } from '../../../../../services/backend.service';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
+// Criando a Interface aqui por enquanto
+interface Resposta {
+  marcada: boolean;
+  opcao: string;
+}
 
 @Component({
   selector: 'app-cursos',
-  imports: [],
+  imports: [ FormsModule, MatInputModule, MatFormFieldModule, CommonModule, FormsModule],
   templateUrl: './cadastrar-quizz.component.html',
   styleUrl: './cadastrar-quizz.component.css'
 })
+
+
 export class CadastrarQuizzComponent implements OnInit {
 
   router = inject(Router)
+     // Array que armazenará as opções de resposta
+     respostas: Resposta[] = [];
+     tipoPergunta: string = ''; // Será "multipla" ou "unica"
+
 
   constructor(private backEnd: BackendService, private dialog: MatDialog){
 
@@ -25,6 +42,19 @@ export class CadastrarQuizzComponent implements OnInit {
       })
   }
 
+
+  // Método para adicionar uma nova resposta
+  adicionarResposta(): void {
+    // Adiciona um objeto com valores padrão
+    this.respostas.push({ marcada: false, opcao: '' });
+  }
+
+  // Método para remover uma resposta
+  removerResposta(index: number): void {
+    this.respostas.splice(index, 1);
+  }
+
+
   editarCurso(){
     this.router.navigate(['/admin/editar-curso']);
   }
@@ -33,4 +63,5 @@ export class CadastrarQuizzComponent implements OnInit {
     this.router.navigate(['/admin/cadastro-curso']);
 
    }
+
 }
