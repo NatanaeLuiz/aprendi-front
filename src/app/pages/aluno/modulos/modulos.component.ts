26a5af82d4995673b6fdfbba24bc581774c5092c
Natanael Rolim <natanael.10@hotmail.com>
2025-02-15 09:50:05 -0300

    FEAT: Create modal

--- src/app/pages/aluno/modulos/modulos.component.ts
+++ src/app/pages/aluno/modulos/modulos.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { NavModuloComponent } from './nav-modulo/nav-modulo.component';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
+import { ModalComponent } from '../../../components/modal/modal.component';
 
 @Component({
   selector: 'app-modulos',
-  imports: [NavModuloComponent, NgFor],
+  imports: [NavModuloComponent, NgFor, NgIf, ModalComponent, NgTemplateOutlet],
   templateUrl: './modulos.component.html',
   styleUrl: './modulos.component.css'
 })
 export class ModulosComponent {
   modulos: String[] = ['Teste1', 'Teste2', 'Teste3', 'Teste4', 'teste5']
 
+  isModalOpen = false;
+
+  openModal() {
+    this.isModalOpen = true;
+    console.log('Modal Aberto:', this.isModalOpen);
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }
